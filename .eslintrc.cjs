264c74f67c9656d45fcf7c9492df79126efc75cd
pontush81 <149173481+pontush81@users.noreply.github.com>
2025-05-17 10:30:44 +0200

    Fix ESLint configuration: Downgrade to ESLint v8.57.0, remove eslint.config.js, update .eslintrc.cjs with Next.js support

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,13 +5,16 @@ module.exports = {
     project: ['./tsconfig.json'],
     tsconfigRootDir: __dirname,
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', '@next/next'],
   extends: [
     'plugin:@typescript-eslint/recommended',
+    'plugin:@next/next/recommended',
   ],
   rules: {
     '@typescript-eslint/no-unused-vars': 'warn',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-require-imports': 'off',
+    '@next/next/no-html-link-for-pages': 'warn',
+    '@next/next/no-img-element': 'warn',
   },
 }; 
