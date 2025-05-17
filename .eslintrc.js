c13833ce62d77c4e2f951a47450e0209231fc787
pontush81 <149173481+pontush81@users.noreply.github.com>
2025-05-17 10:15:35 +0200

    Improve ESLint configuration with TypeScript parser and additional options

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
   extends: [
-    'next/core-web-vitals',
     'plugin:@typescript-eslint/recommended',
   ],
   rules: {
